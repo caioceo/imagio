9a48079e7c2d906af6be49766080242f556bdff2
caioceo <caioceo06@gmail.com>
2025-07-07 16:48:28 -0300

    init cookies session and loggout endpoint #2

---
+++ src/routes/api/user/delete/+server.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import type { RequestHandler } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+
+        const formData = await request.formData();
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+        
+        if (
+            typeof email !== 'string' ||
+            typeof password !== 'string' ||
+            !email || !password
+        ) {
+            console.log('incorrect data')
+            return new Response(
+                JSON.stringify({ success: false }),
+                { status: 401 }
+            );
+        }
+
+        const userID = await prisma.user.findUnique({ where: { email } });
+
+        if (!userID) {
+            return new Response(
+                JSON.stringify({ success: false}),
+                { status: 409 }
+            );
+        }
+
+        const passwordVerify = await bcrypt.compare(password, userID.password)
+
+        if (!passwordVerify){
+            console.log('email or password are incorrect')
+            return new Response(JSON.stringify({ success: false}), { status: 501 });
+        }
+        else{
+            await prisma.user.delete({where: {id: userID.id}})
+            cookies.delete('auth_token', { path: '/' });
+            console.log("successful")
+            return new Response(JSON.stringify({ success: true}), { status: 201});
+        }
+};
+
--- src/routes/api/user/login/+server.ts
+++ src/routes/api/user/login/+server.ts
@@ -4,43 +4,49 @@ import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request, cookies }) => {
 
-        const formData = await request.formData();
+    const formData = await request.formData();
 
-        const email = formData.get('email');
-        const password = formData.get('password');
-        
-        if (
-            typeof email !== 'string' ||
-            typeof password !== 'string' ||
-            !email || !password
-        ) {
-            console.log('incorrect data')
-            return new Response(
-                JSON.stringify({ success: false }),
-                { status: 401 }
-            );
-        }
+    const email = formData.get('email');
+    const password = formData.get('password');
 
-        const userID = await prisma.user.findUnique({ where: { email } });
+    if (
+        typeof email !== 'string' ||
+        typeof password !== 'string' ||
+        !email || !password
+    ) {
+        console.log('incorrect data')
+        return new Response(
+            JSON.stringify({ success: false }),
+            { status: 401 }
+        );
+    }
 
-        if (!userID) {
-            return new Response(
-                JSON.stringify({ success: false}),
-                { status: 409 }
-            );
-        }
+    const userID = await prisma.user.findUnique({ where: { email } });
 
-        const passwordVerify = await bcrypt.compare(password, userID.password)
+    if (!userID) {
+        return new Response(
+            JSON.stringify({ success: false}),
+            { status: 409 }
+        );
+    }
 
-        if (!passwordVerify){
-            console.log('email or password are incorrect')
-            return new Response(JSON.stringify({ success: false}), { status: 501 });
-        }
-        else{
-            console.log("successful login")
-            return new Response(JSON.stringify({ success: true}), { status: 201});
-        }
+    const passwordVerify = await bcrypt.compare(password, userID.password)
+
+    if (!passwordVerify) {
+        console.log('email or password are incorrect')
+        return new Response(JSON.stringify({ success: false }), { status: 409 });
+    }
+    else {
+        cookies.set('auth_token', userID.id, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 3600 * 24 * 30
+        });
+        console.log("successful login")
+        return new Response(JSON.stringify({ success: true }), { status: 201 });
+    }
 };
 
